10af0a30293c4a2a70244d684f495e491d7c9e69
Heungrok Park <mabookyggl@gmail.com>
2026-08-04 19:39:59 +0900

    perf(galerie-de-bal): Google Fonts Material Design 3 Icons 로딩 최적화

--- apps/galerie-de-bal/src/app/layout.tsx
+++ apps/galerie-de-bal/src/app/layout.tsx
@@ -28,7 +28,8 @@ export default function RootLayout({
         >
             <head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=chevron_left,chevron_right,close,pause,play_arrow&display=block" />
             </head>
             <body className="w-full h-full flex flex-col m-0 p-0">
                 <BgmProvider>
