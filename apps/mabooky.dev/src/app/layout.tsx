1d09895b72d38acbc1e20db908eb668aa8929227
Heungrok Park <mabookyggl@gmail.com>
2026-08-04 20:02:09 +0900

    perf(mabooky.dev): 사용하지 않는 Roboto 폰트 로딩 제거 및 Material Icons 로딩 최적화

--- apps/mabooky.dev/src/app/layout.tsx
+++ apps/mabooky.dev/src/app/layout.tsx
@@ -17,8 +17,7 @@ export default function RootLayout({
             <head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"/>
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"/>
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=brightness_auto,dark_mode,email,light_mode&display=block"/>
             </head>
             <body className="w-full h-full">
                 <MD3Provider
