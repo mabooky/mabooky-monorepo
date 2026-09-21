f351e0929af4877560b02f737d1789346fde2a84
Heungrok Park <mabookyggl@gmail.com>
2026-09-22 00:45:48 +0900

    feat(mabooky.dev): MDX 지원 추가
    - 추가로 @next/mdx 패키지와의 next 버전 차이로 인한 문제 해결을 위해 mabooky.dev의 next 버전 업데이트

--- apps/mabooky.dev/next.config.ts
+++ apps/mabooky.dev/next.config.ts
@@ -1,11 +1,22 @@
 import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
+import remarkGfm from "remark-gfm";
+import rehypeSlug from "rehype-slug";
+import rehypePrettyCode from "rehype-pretty-code";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
     output: 'export',
     reactCompiler: true,
     transpilePackages: ['@mabooky/md3',],
     allowedDevOrigins: ['172.30.1.*', '192.168.113.*'],
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+    options: {
+        remarkPlugins: [remarkGfm],
+        rehypePlugins: [rehypeSlug, [rehypePrettyCode, { theme: 'github-dark' }]],
+    }
+})
+
+export default withMDX(nextConfig);
